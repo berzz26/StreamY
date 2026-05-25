d0e38b2b304cc816d8ec9e6fc88638a5898e809e
berzz26 <aumtamboli2233@gmail.com>
2026-05-25 11:42:13 +0000

    feat: add database configuration and connection handling

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	DatabaseUrl string
+}
+
+func LoadDb() *Config {
+	dbUrl := os.Getenv("DATABASE_URL")
+
+	if dbUrl == "" {
+		log.Fatal("DATABASE_URL is missing")
+	}
+	return &Config{
+		DatabaseUrl: dbUrl,
+	}
+}
