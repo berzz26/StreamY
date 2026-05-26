3a482d888a5f04f5fb18c537e6d02abc4bfa667d
berzz26 <aumtamboli2233@gmail.com>
2026-05-26 06:04:41 +0000

    refactor: update config loading functions and add port handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	
-	"time"
+	// "context"
+	// "fmt"
+
+	// "time"
 
 	"github.com/berzz26/StreamY/internal/config"
-	"github.com/berzz26/StreamY/internal/database"
+	// "github.com/berzz26/StreamY/internal/database"
+	"github.com/berzz26/StreamY/internal/streaming"
+	"github.com/gofiber/fiber/v2"
+	"log"
 )
 
 func main() {
 
-	cfg := config.LoadDb()
-	db := database.New(cfg.DatabaseUrl)
-	
-	defer db.Close()
+	// cfg := config.LoadDb()
+	// db := database.New(cfg.DatabaseUrl)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
+	// defer db.Close()
 
-	var version string
+	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// defer cancel()
 
-	err := db.DB.QueryRow(ctx, "SELECT version()").Scan(&version)
-	if err != nil {
-		panic(err)
+	// var version string
 
-	}
+	// err := db.DB.QueryRow(ctx, "SELECT version()").Scan(&version)
+	// if err != nil {
+	// 	panic(err)
 
-	fmt.Println(version)
+	// }
+
+	// fmt.Println(version)
+
+	app := fiber.New()
+
+	streaming.RegisterRoutes(app)
+	log.Printf("Api server up on %s")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -8,19 +8,26 @@ import (
 
 type Config struct {
 	DatabaseUrl string
+	Port        string
 }
 
-func LoadDb() *Config {
+func LoadConfig() *Config {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(".env file not found")
 	}
 	dbUrl := os.Getenv("DATABASE_URL")
+	port := os.Getenv("PORT")
 
 	if dbUrl == "" {
 		log.Fatal("DATABASE_URL is missing")
 	}
+
+	if port == "" {
+		log.Fatal("Port is missing")
+	}
 	return &Config{
 		DatabaseUrl: dbUrl,
+		Port:        port,
 	}
 }
