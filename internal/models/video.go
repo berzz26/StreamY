2515b4b501764008e0908d533fd09baba0fa1f8e
berzz26 <aumtamboli2233@gmail.com>
2026-05-26 07:16:43 +0000

    feat: implement the video model

--- internal/models/video.go
+++ internal/models/video.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+type Video struct {
+	ID            string
+	Title         string
+	Status        string
+	OriginalPath  string
+	ProcessedPath string
+	ErrorMessage  string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
