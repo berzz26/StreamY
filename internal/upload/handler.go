5b12b2bc5cc2b6ef7d44ed3ba794ec17cf12a9c5
berzz26 <aumtamboli2233@gmail.com>
2026-05-26 06:56:41 +0000

    feat: implement video upload functionality with file handling and response

--- internal/upload/handler.go
+++ internal/upload/handler.go
@@ -1,1 +1,52 @@
 package upload
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+func RegisterRoutes(app *fiber.App) {
+
+	app.Post("/upload", UploadVideo)
+}
+
+func UploadVideo(c *fiber.Ctx) error {
+
+	file, err := c.FormFile("video")
+	if err != nil {
+		return fiber.NewError(
+			fiber.StatusBadRequest,
+			"video file required",
+		)
+	}
+
+	videoID := uuid.New().String()
+
+	err = os.MkdirAll("./uploads", os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	extension := filepath.Ext(file.Filename)
+
+	localPath := fmt.Sprintf(
+		"./uploads/%s%s",
+		videoID,
+		extension,
+	)
+
+	err = c.SaveFile(file, localPath)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(fiber.Map{
+		"video_id": videoID,
+		"path":     localPath,
+		"status":   "uploaded",
+	})
+}
